33d9707725d4235da53e45ab8322650c6ba2a2fe
coletivadotidico-del <coletivadotidico@gmail.com>
2026-06-04 01:28:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const cors = require('cors');
-app.use(cors()); // Isso libera o site do Netlify para chamar o seu servidor
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
